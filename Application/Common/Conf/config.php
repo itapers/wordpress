00fcefd421892d824737d95d49b170090d279bc5
wangpenglei <15649012981@163.com>
2017-08-11 18:17:45 +0800

    修改

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
     //'配置项'=>'配置值'
         'DB_TYPE'   => 'mysql', // 数据库类型
-        'DB_HOST'   => '127.0.0.1', // 服务器地址
+        'DB_HOST'   => '47.93.217.133', // 服务器地址
         'DB_NAME'   => 'thinkblog', // 数据库名
-        'DB_USER'   => 'root', // 用户名
-        'DB_PWD'    => 'root',  // 密码
+        'DB_USER'   => 'thinkblog', // 用户名
+        'DB_PWD'    => 'LEj53jMZZd',  // 密码
         'DB_PORT'   => '3306', // 端口
         'DB_PREFIX' => 'blog_', // 数据库表前缀
         'SHOW_PAGE_TRACE' =>false,  //
